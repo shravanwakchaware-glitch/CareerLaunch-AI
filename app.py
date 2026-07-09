f2651bb06846e93c0363f53b10c5ac5496c2d5d9
shravanwakchaware-glitch <shravanwakchaware@gmail.com>
2026-07-09 20:30:21 +0530

    Implement user sessions and logout

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,session
 import sqlite3
 app = Flask(__name__)
+app.secret_key = "careerlaunch_secret_key"
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -25,6 +26,9 @@ def login():
         connection.close()
 
         if user:
+            session["user_id"] = user[0]
+            session["username"] = user[1]
+            session["email"] = user[2]
             return redirect(url_for("dashboard"))
         else:
             return "Invalid Email or Password"
@@ -55,6 +59,17 @@ def signup():
     return render_template("signup.html")
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    return render_template(
+        "dashboard.html",
+        username=session["username"]
+    )
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
 if __name__ == '__main__':
     app.run(debug=True)
